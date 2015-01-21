68ae6aa557520cbcf3261ed8c351756212990d50
james-driscoll <james.e.m.driscoll@gmail.com>
2015-01-21 00:20:15 +0000

    Added columns to Project table.

--- MyTeamData/Project.sql
+++ MyTeamData/Project.sql
@@ -3,5 +3,9 @@
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [Title] NVARCHAR(50) NULL, 
     [Description] NVARCHAR(MAX) NULL, 
-    [Status] NVARCHAR(50) NULL
+    [Status] NVARCHAR(50) NULL, 
+    [PercentageCompleted] INT NULL, 
+    [StartDate] DATE NULL, 
+    [EndDate] DATE NULL, 
+    [Duration] INT NULL
 )
