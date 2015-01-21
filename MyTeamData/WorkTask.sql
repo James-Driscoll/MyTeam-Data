a0c6621e59fc7206b0d61a739db10871b74ebee9
james-driscoll <james.e.m.driscoll@gmail.com>
2015-01-21 00:02:56 +0000

    Added columns to the WorkTask table.

--- MyTeamData/WorkTask.sql
+++ MyTeamData/WorkTask.sql
@@ -2,5 +2,11 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
     [Name] NVARCHAR(50) NULL, 
-    [Description] NVARCHAR(MAX) NULL
+    [Description] NVARCHAR(MAX) NULL, 
+    [Status] NVARCHAR(50) NULL, 
+    [PercentageCompleted] INT NULL, 
+    [EstimatedDuration] INT NULL, 
+    [StartDate] DATE NULL, 
+    [EndDate] DATE NULL, 
+    [Duration] INT NULL
 )
