84f2fa1340a7477a33406d6a13757ff0efba75d6
james-driscoll <james.e.m.driscoll@gmail.com>
2015-02-08 20:31:44 +0000

    Updated User table.

---
+++ MyTeamData/User.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [FirstName] NVARCHAR(50) NULL, 
+    [Lastname] NVARCHAR(MAX) NULL, 
+    [Email] NVARCHAR(MAX) NOT NULL, 
+    [Street] NVARCHAR(MAX) NULL, 
+    [Town] NVARCHAR(MAX) NULL, 
+    [County] NVARCHAR(MAX) NULL, 
+    [Postcode] NVARCHAR(50) NULL, 
+    [MobileNumber] NVARCHAR(50) NULL, 
+    [DOB] DATE NULL, 
+    [SystemRole] NVARCHAR(50) NULL
+)
