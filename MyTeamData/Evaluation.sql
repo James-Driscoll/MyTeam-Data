efe3e27b203e4b4e0cdfaa155592aa659c8957d0
james-driscoll <james.e.m.driscoll@gmail.com>
2015-02-16 10:54:31 +0000

    Updated Task foreign key for Evaluation table.

--- MyTeamData/Evaluation.sql
+++ MyTeamData/Evaluation.sql
@@ -2,9 +2,9 @@
 (
 	[PK_EvaluationID] INT NOT NULL PRIMARY KEY IDENTITY, 
     [FK_Assessor] INT NOT NULL, 
-    [FK_Task] INT NOT NULL, 
-    [Mark] INT NULL, 
+    [FK_WorkTask] INT NOT NULL, 
+    [Mark] INT NOT NULL, 
     [Comments] NVARCHAR(MAX) NULL, 
     CONSTRAINT [C_Assessor] FOREIGN KEY ([FK_Assessor]) REFERENCES [dbo].[User]([PK_UserID]),
-	CONSTRAINT [C_Task] FOREIGN KEY ([FK_Task]) REFERENCES [dbo].[WorkTask]([PK_WorkTaskID])
+	CONSTRAINT [C_Task] FOREIGN KEY ([FK_WorkTask]) REFERENCES [dbo].[WorkTask]([PK_WorkTaskID])
 )
