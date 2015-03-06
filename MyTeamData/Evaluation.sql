1e9e0f8c66da024c92486618e613fc77e0715aa2
james-driscoll <james.e.m.driscoll@gmail.com>
2015-03-06 00:41:10 +0000

    Corrected FK column name.

--- MyTeamData/Evaluation.sql
+++ MyTeamData/Evaluation.sql
@@ -2,7 +2,7 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [FK_Assessor] INT NOT NULL, 
-    [FK_WorkTask] INT NOT NULL, 
+    [FK_Task] INT NOT NULL, 
     [Mark] INT NOT NULL, 
     [Comments] NVARCHAR(50) NULL
 )
